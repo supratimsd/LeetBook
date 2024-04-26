93c0fd9f21fe4d4a7837785830e0411bde818495
Supratim Dey <115398638+supratimsd@users.noreply.github.com>
2024-04-27 01:16:26 +0530

    Time: 16 ms (79.08%), Space: 45.3 MB (25.74%) - LeetHub

---
+++ 0459-repeated-substring-pattern/0459-repeated-substring-pattern.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean repeatedSubstringPattern(String s) {
+        int n=s.length();
+        for(int i=n/2;i>=1;i--){
+            if(n%i==0){
+                String substr=s.substring(0,i);
+                StringBuilder sb=new StringBuilder();
+                for(int j=1;j<=n/i;j++)
+                    sb.append(substr);
+                if(sb.toString().equals(s))
+                    return true;
+            }
+        }
+        return false;
+    }
+}
