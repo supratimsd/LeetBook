2793cb0588280c6ead11808f95499849ed489067
Supratim Dey <115398638+supratimsd@users.noreply.github.com>
2024-08-01 20:59:57 +0530

    Time: 0 ms (100.00%), Space: 42.7 MB (10.74%) - LeetHub

---
+++ 0162-find-peak-element/0162-find-peak-element.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int findPeakElement(int[] nums) {
+        int n=nums.length;
+        int ans=0;
+        int low=0;
+        int high=n-1;
+        while(low<high){
+            int mid=low+(high-low)/2;
+            if(nums[mid]<nums[mid+1]){
+                low=mid+1;
+            }
+            else{
+                high=mid;
+            }
+        }
+        return low;
+    }
+}
