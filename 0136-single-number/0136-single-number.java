692736547c1d75761ed790a551d1a3b9c62c030b
Supratim Dey <115398638+supratimsd@users.noreply.github.com>
2024-09-06 16:24:10 +0530

    Time: 207 ms (9.47%), Space: 45.9 MB (62.95%) - LeetHub

---
+++ 0136-single-number/0136-single-number.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        int n=nums.length;
+        for(int i=0;i<n;i++){
+	        int flag=0;
+	        for(int j=0;j<n;j++){
+	            if(i!=j && nums[i]==nums[j]){
+	                flag=1;
+	                break;
+	            }
+	        }
+	        if (flag==0) {
+                return nums[i];
+                // break; 
+            }
+	    }
+	    return -1;
+    }
+}
