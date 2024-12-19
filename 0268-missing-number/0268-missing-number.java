f82f24960d767472c3918ca42641e6119c99bc05
Supratim Dey <115398638+supratimsd@users.noreply.github.com>
2024-12-19 12:10:02 +0530

    Update 0268-missing-number.java

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -8,23 +8,7 @@ class Solution {
              sdsum +=nums[u];
         }
         return sdtot-sdsum;
-//         int e=nums.length;
-//         Arrays.sort(nums);
-//         // int p=0;
-//         if(e==1 && nums[e-1]==0){
-//             return 1;
-//         }
-//         if(e==1 && nums[e-1]==1){
-//             return 0;
-//         }
-        
-//         for(int s=0;s<e-1;s++){
-//             if((nums[s]+1)!=nums[s+1]){
-//                  return nums[s]+1;
-                
-//             }
-//         }
-//         return e;
+
         
     }
 }
