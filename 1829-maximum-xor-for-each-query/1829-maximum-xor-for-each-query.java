aa2d780bfc76c0d2ce102527add79868f85d59e2
Supratim Dey <115398638+supratimsd@users.noreply.github.com>
2024-12-23 12:48:49 +0530

    Time: 3 ms (53.35%), Space: 58.8 MB (44.23%) - LeetHub

--- 1829-maximum-xor-for-each-query/1829-maximum-xor-for-each-query.java
+++ 1829-maximum-xor-for-each-query/1829-maximum-xor-for-each-query.java
@@ -1,19 +1,34 @@
 class Solution {
     public int[] getMaximumXor(int[] nums, int maximumBit) {
-        int n = nums.length;
-        int xorr = nums[0];
-        int maxXor = (1 << maximumBit) - 1;
-
-        for (int i = 1; i < n; i++) {
-            xorr ^= nums[i];
+        int s=nums.length;
+        int[] sd=new int[s];
+        
+        int sdxor=0;
+        int maxXor = (1 << maximumBit) - 1; 
+        if(s==1 ){
+                sd[0]=nums[0]^maxXor;
+                return sd;
         }
-
-        int[] ans = new int[n];
-        for (int i = 0; i < n; i++) {
-            ans[i] = xorr ^ maxXor;
-            xorr ^= nums[n - 1 - i];
+        for(int y=0;y<s;y++){
+            
+            sdxor ^=nums[y];
+            sd[y]=sdxor^maxXor;
         }
-
-        return ans;
+        
+        
+        
+        int left=0;
+        int right=s-1;
+        while(left<right){
+            int temp=sd[left];
+            sd[left]=sd[right];
+            sd[right]=temp;
+            left++;
+            right--;
+        }
+        
+        return sd;
+        
+        
     }
 }
