ae644191ae0c13d0c71b8fa5402d9d365d522c7c
Supratim Dey <115398638+supratimsd@users.noreply.github.com>
2024-12-27 20:18:02 +0530

    1717-maximum-score-from-removing-substrings.java

--- 1717-maximum-score-from-removing-substrings/1717-maximum-score-from-removing-substrings.java
+++ 1717-maximum-score-from-removing-substrings/1717-maximum-score-from-removing-substrings.java
@@ -1,43 +1,44 @@
 class Solution {
     public int maximumGain(String s, int x, int y) {
-        int res = 0;
-        String top, bot;
-        int top_score, bot_score;
-
-        if (y > x) {
-            top = "ba";
-            top_score = y;
-            bot = "ab";
-            bot_score = x;
-        } else {
-            top = "ab";
-            top_score = x;
-            bot = "ba";
-            bot_score = y;
+        if(y>x)
+        {
+           return helper(s,y,x,'b','a');
         }
+        else
+        {
+          return helper(s,x,y,'a','b');
+        }
+    }
 
-        // Removing first top substrings cause they give more points
-        StringBuilder stack = new StringBuilder();
-        for (char ch : s.toCharArray()) { // Changed 'char' to 'ch'
-            if (ch == top.charAt(1) && stack.length() > 0 && stack.charAt(stack.length() - 1) == top.charAt(0)) {
-                res += top_score;
-                stack.setLength(stack.length() - 1);
-            } else {
-                stack.append(ch);
+   public int helper(String s,int high,int low,char first,char second)
+    {
+        StringBuilder sb=new StringBuilder();
+        int total=0;
+
+        for(char c:s.toCharArray())
+        {
+            if(sb.length()>0 && sb.charAt(sb.length()-1)==first && c==second)
+            {
+                sb.deleteCharAt(sb.length()-1);
+                total+=high;
+            }
+            else {
+                sb.append(c);
+            }
+        }
+        StringBuilder result=new StringBuilder();
+        for(char c:sb.toString().toCharArray())
+        {
+            if(result.length()>0 && result.charAt(result.length()-1)==second && c==first)
+            {
+                result.deleteCharAt(result.length()-1);
+                total+=low;
+            }
+            else {
+                result.append(c);
             }
         }
 
-        // Removing bot substrings cause they give less or equal amount of scores
-        StringBuilder new_stack = new StringBuilder();
-        for (char ch : stack.toString().toCharArray()) { // Changed 'char' to 'ch'
-            if (ch == bot.charAt(1) && new_stack.length() > 0 && new_stack.charAt(new_stack.length() - 1) == bot.charAt(0)) {
-                res += bot_score;
-                new_stack.setLength(new_stack.length() - 1);
-            } else {
-                new_stack.append(ch);
-            }
-        }
-
-        return res;
+        return total;
     }
 }
