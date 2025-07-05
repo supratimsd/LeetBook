e163319ac5e4d94008359c397c46848affef58f6
Supratim Dey <115398638+supratimsd@users.noreply.github.com>
2025-07-05 23:37:06 +0530

    Time: 5 ms (72.1%), Space: 43.5 MB (33.08%) - LeetHub

--- 1394-find-lucky-integer-in-an-array/1394-find-lucky-integer-in-an-array.java
+++ 1394-find-lucky-integer-in-an-array/1394-find-lucky-integer-in-an-array.java
@@ -1,27 +1,21 @@
 class Solution {
     public int findLucky(int[] arr) {
-        int n=arr.length;
-        int majority=1;
-        int count1=1;
-        int count2=-1;
-        int res=0;
-        Arrays.sort(arr);
-        for(int i=0;i<n;i++){
-            if( i<n-1 && arr[i]==arr[i+1]){
-                count1++;
-            }else{
-                
-            if(count1==arr[i]){
-            // System.out.println(" "+count);
-            count2= arr[i];
-            
-            }
-            count1=1;
-        
-        
-        
-    }
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        // Count frequency of each element
+        for (int num : arr) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
         }
-    return count2;
+
+        int max = -1;
+
+        // Check if number == its frequency
+        for (int key : map.keySet()) {
+            if (key == map.get(key)) {
+                max = Math.max(max, key);
+            }
+        }
+
+        return max;
     }
 }
